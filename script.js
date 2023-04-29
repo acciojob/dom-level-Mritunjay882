adda1f3a5ed6f9152d038e83df35928536a7a527
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-29 09:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var element = document.getElementById("level");
+
+// Initialize a counter variable to keep track of the number of levels
+var level = 0;
+
+// Traverse up the DOM tree until we reach the body element
+while (element.parentNode.tagName !== "BODY") {
+  element = element.parentNode;
+  level++;
+}
+
+// Display the result using alert()
+alert("The level of the element is: " + level);
